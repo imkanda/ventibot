e6683edf4f583a338b8dd8e77aaa63a038fae136
Luca R.W <57198343+axen0s@users.noreply.github.com>
2021-08-05 11:40:31 -0400

    Refactor basically all the code that has anything to do with scraping manga

--- mangascrape.py
+++ mangascrape.py
@@ -1,229 +1,46 @@
 from bs4 import BeautifulSoup
 import requests
+import json
 
 
 class Scrape:
-    def jjk_chapter_check(self):
-        jjkxml = 'https://mangasee123.com/rss/Jujutsu-Kaisen.xml'
-        source = requests.get(jjkxml).text
-        soup = BeautifulSoup(source, 'lxml')
-        contents = soup.find_all('title')
-        new_chp = contents[2].text
-        return new_chp
+    def __init__(self):
+        with open("manga.json") as local_manga_file:
+            self.manga = json.load(local_manga_file)
 
-    def jjk_link(self):
-        jjkxml = 'https://mangasee123.com/rss/Jujutsu-Kaisen.xml'
-        source = requests.get(jjkxml).text
-        soup = BeautifulSoup(source, features='xml')
-        contents = soup.find_all('link')
-        chp_link = contents[3].text
-        return chp_link
+    def manga_info(self, id):  # get newest ch and ch link for a manga id in our json file
+        xml_link = self.manga[id]["source"]
+        xml = requests.get(xml_link).text
+        soup = BeautifulSoup(xml, 'lxml')
+        titles = soup.find_all('title')
+        new_ch = titles[2].text
+        links = soup.find_all('link')
+        ch_link = links[3].text
+        return new_ch, ch_link
 
-    def csm_chapter_check(self):
-        csmxml = 'https://mangasee123.com/rss/Chainsaw-Man.xml'
-        source = requests.get(csmxml).text
-        soup = BeautifulSoup(source, 'lxml')
-        contents = soup.find_all('title')
-        new_chp = contents[2].text
-        return new_chp
+    def new_ch_check(self):  # iterates over manga.json and checks if any have new chapters
+        _manga = []
+        for manga_id in self.manga:
+            if manga_id != "info":
+                _manga_info = self.manga[manga_id]
+                new_ch, ch_link = self.manga_info(manga_id)
+                desc = self.manga['info']['default_desc'].replace("%title%", _manga_info['title'])
+                if "desc" in _manga_info:
+                    desc = _manga_info['desc']
+                if "local_ch" in _manga_info:
+                    if new_ch != _manga_info["local_ch"]:
+                        if "ignore" in _manga_info:
+                            if not _manga_info["ignore"]:
+                                _manga.append({"ch": new_ch, "link": ch_link, "desc": desc, "thumbnail": _manga_info["thumbnail"]})
+                else:
+                    print(f"{_manga_info['title']} has never been scraped, syncing its chapter count to current")
+                    self.update_local_ch(manga_id, new_ch)
+        return _manga
 
-    def csm_link(self):
-        csmxml = 'https://mangasee123.com/rss/Chainsaw-Man.xml'
-        source = requests.get(csmxml).text
-        soup = BeautifulSoup(source, features='xml')
-        contents = soup.find_all('link')
-        chp_link = contents[3].text
-        return chp_link
+    def save_manga_info(self):  # updates local json file to whatever self.manga is now (should only ever be ch update)
+        with open("manga.json", "w") as local_manga_file:
+            json.dump(self.manga, local_manga_file, indent=2)
 
-    def snk_chapter_check(self):
-        snkxml = 'https://mangasee123.com/rss/Shingeki-No-Kyojin.xml'
-        source = requests.get(snkxml).text
-        soup = BeautifulSoup(source, 'lxml')
-        contents = soup.find_all('title')
-        new_chp = contents[2].text
-        return new_chp
-
-    def snk_link(self):
-        snkxml = 'https://mangasee123.com/rss/Shingeki-No-Kyojin.xml'
-        source = requests.get(snkxml).text
-        soup = BeautifulSoup(source, features='xml')
-        contents = soup.find_all('link')
-        chp_link = contents[3].text
-        return chp_link
-
-    def sl_chapter_check(self):
-        slxml = 'https://mangasee123.com/rss/Solo-Leveling.xml'
-        header = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36'}
-        source = requests.get(slxml, headers=header).text
-        soup = BeautifulSoup(source, 'lxml')
-        contents = soup.find_all('title')
-        new_chp = contents[2].text
-        return new_chp
-
-    def sl_link(self):
-        slxml = 'https://mangasee123.com/rss/Solo-Leveling.xml'
-        source = requests.get(slxml).text
-        soup = BeautifulSoup(source, features='xml')
-        contents = soup.find_all('link')
-        chp_link = contents[3].text
-        return chp_link
-
-    def brsrk_chapter_check(self):
-        brsrkxml = 'https://mangasee123.com/rss/Berserk.xml'
-        source = requests.get(brsrkxml).text
-        soup = BeautifulSoup(source, 'lxml')
-        contents = soup.find_all('title')
-        new_chp = contents[2].text
-        return new_chp
-
-    def brsrk_link(self):
-        brsrkxml = 'https://mangasee123.com/rss/Berserk.xml'
-        source = requests.get(brsrkxml).text
-        soup = BeautifulSoup(source, features='xml')
-        contents = soup.find_all('link')
-        chp_link = contents[3].text
-        return chp_link
-
-    def bc_chapter_check(self):
-        bcxml = 'https://mangasee123.com/rss/Black-Clover.xml'
-        source = requests.get(bcxml).text
-        soup = BeautifulSoup(source, 'lxml')
-        contents = soup.find_all('title')
-        new_chp = contents[2].text
-        return new_chp
-
-    def bc_link(self):
-        bcxml = 'https://mangasee123.com/rss/Black-Clover.xml'
-        source = requests.get(bcxml).text
-        soup = BeautifulSoup(source, features='xml')
-        contents = soup.find_all('link')
-        chp_link = contents[3].text
-        return chp_link
-
-    def tog_chapter_check(self):
-        togxml = 'https://mangasee123.com/rss/Tower-Of-God.xml'
-        source = requests.get(togxml).text
-        soup = BeautifulSoup(source, 'lxml')
-        contents = soup.find_all('title')
-        new_chp = contents[2].text
-        return new_chp
-
-    def tog_link(self):
-        togxml = 'https://mangasee123.com/rss/Tower-Of-God.xml'
-        source = requests.get(togxml).text
-        soup = BeautifulSoup(source, features='xml')
-        contents = soup.find_all('link')
-        chp_link = contents[3].text
-        return chp_link
-
-    def op_chapter_check(self):
-        opxml = 'https://mangasee123.com/rss/One-Piece.xml'
-        source = requests.get(opxml).text
-        soup = BeautifulSoup(source, 'lxml')
-        contents = soup.find_all('title')
-        new_chp = contents[2].text
-        return new_chp
-
-    def op_link(self):
-        opxml = 'https://mangasee123.com/rss/One-Piece.xml'
-        source = requests.get(opxml).text
-        soup = BeautifulSoup(source, features='xml')
-        contents = soup.find_all('link')
-        chp_link = contents[3].text
-        return chp_link
-
-    def boxer_chapter_check(self):
-        boxerxml = 'https://mangasee123.com/rss/The-Boxer.xml'
-        source = requests.get(boxerxml).text
-        soup = BeautifulSoup(source, 'lxml')
-        contents = soup.find_all('title')
-        new_chp = contents[2].text
-        return new_chp
-
-    def boxer_link(self):
-        boxerxml = 'https://mangasee123.com/rss/The-Boxer.xml'
-        source = requests.get(boxerxml).text
-        soup = BeautifulSoup(source, features='xml')
-        contents = soup.find_all('link')
-        chp_link = contents[3].text
-        return chp_link
-
-    def bl_chapter_check(self):
-        blxml = 'https://mangasee123.com/rss/Blue-Lock.xml'
-        source = requests.get(blxml).text
-        soup = BeautifulSoup(source, 'lxml')
-        contents = soup.find_all('title')
-        new_chp = contents[2].text
-        return new_chp
-
-    def bl_link(self):
-        blxml = 'https://mangasee123.com/rss/Blue-Lock.xml'
-        source = requests.get(blxml).text
-        soup = BeautifulSoup(source, features='xml')
-        contents = soup.find_all('link')
-        chp_link = contents[3].text
-        return chp_link
-
-    def BATE_chapter_check(self):
-        BATExml = 'https://mangasee123.com/rss/The-Beginning-After-The-End.xml'
-        source = requests.get(BATExml).text
-        soup = BeautifulSoup(source, 'lxml')
-        contents = soup.find_all('title')
-        new_chp = contents[2].text
-        return new_chp
-
-    def BATE_link(self):
-        BATExml = 'https://mangasee123.com/rss/The-Beginning-After-The-End.xml'
-        source = requests.get(BATExml).text
-        soup = BeautifulSoup(source, features='xml')
-        contents = soup.find_all('link')
-        chp_link = contents[3].text
-        return chp_link
-
-    def OPM_chapter_check(self):
-        OPMxml = 'https://mangasee123.com/rss/Onepunch-Man.xml'
-        source = requests.get(OPMxml).text
-        soup = BeautifulSoup(source, 'lxml')
-        contents = soup.find_all('title')
-        new_chp = contents[2].text
-        return new_chp
-
-    def OPM_link(self):
-        OPMxml = 'https://mangasee123.com/rss/Onepunch-Man.xml'
-        source = requests.get(OPMxml).text
-        soup = BeautifulSoup(source, features='xml')
-        contents = soup.find_all('link')
-        chp_link = contents[3].text
-        return chp_link
-
-    def ddd_chapter_check(self):
-        DDDxml = 'https://mangasee123.com/rss/Dandadan.xml'
-        source = requests.get(DDDxml).text
-        soup = BeautifulSoup(source, 'lxml')
-        contents = soup.find_all('title')
-        new_chp = contents[2].text
-        return new_chp
-
-    def ddd_link(self):
-        DDDxml = 'https://mangasee123.com/rss/Dandadan.xml'
-        source = requests.get(DDDxml).text
-        soup = BeautifulSoup(source, features='xml')
-        contents = soup.find_all('link')
-        chp_link = contents[3].text
-        return chp_link
-
-    def kengan_chapter_check(self):
-        Kenganxml = 'https://mangasee123.com/rss/Kengan-Omega.xml'
-        source = requests.get(Kenganxml).text
-        soup = BeautifulSoup(source, 'lxml')
-        contents = soup.find_all('title')
-        new_chp = contents[2].text
-        return new_chp
-
-    def kengan_link(self):
-        Kenganxml = 'https://mangasee123.com/rss/Kengan-Omega.xml'
-        source = requests.get(Kenganxml).text
-        soup = BeautifulSoup(source, features='xml')
-        contents = soup.find_all('link')
-        chp_link = contents[3].text
-        return chp_link
+    def update_local_ch(self, id, ch):  # update a ch value in self.manga to reflect an updated manga, then save the file
+        self.manga[id]["local_ch"] = ch
+        self.save_manga_info()
